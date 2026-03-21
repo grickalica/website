debb14c45aebd6ee39ea840a266f9a4815bb5c2f
Kiko <piha.tihomir@gmail.com>
2026-03-21 01:16:36 +0100

    logo work

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,9 +21,9 @@
     const logo = document.getElementById('logo');
     if (logo != null) {
       if (window.scrollY > 100) {
-        logo.src = 'assets/img/logo.png';
+        logo.src = 'assets/img/logo.png?v=1';
       } else {
-        logo.src = 'assets/img/logo-white.png';
+        logo.src = 'assets/img/logo-white.png?v=1';
       }
     }
   }
